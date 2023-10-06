7a3a79baf446aaf9fc35f202687aae542f8e4fbe
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-10-06 16:36:32 -0300

    configuração do http

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
 
+const users = []
 const server = http.createServer((req, res) => {
-  res.end("hello")
+  const { method, url } = req
+
+  const buffers = []
+
+  if (method === 'GET' && url === "/users") {
+    return res
+    .setHeader('content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === "/users") {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john@mail.com'
+    })
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end("Not found")
 })
 
 server.listen(3000)
