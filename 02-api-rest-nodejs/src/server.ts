ccb9ab1770406522bf4150c9f5507776789dbc6f
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-10-17 16:24:47 -0300

    configurado eslint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,8 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/', () => 'hello world')
 
 app.listen({ port: 3000 })
-  .then(() => console.log('HTTP Server Running.'))
+  .then(() => { console.log('HTTP Server Running.') })
+  .catch(err => { console.log(err.message) })
