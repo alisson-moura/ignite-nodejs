46cfc9081f9f864cbea92782ea7715344cb67132
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-10-23 17:43:01 -0300

    Configurando ESLint

--- 03-api-nodejs-com-solid/src/server.ts
+++ 03-api-nodejs-com-solid/src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 app.listen({
   host: '0.0.0.0',
   port: env.HTTP_PORT
 })
-  .then(() => { console.log(`API running on http://localhost:${env.HTTP_PORT}`) })
+  .then(() => {
+    console.log(`API running on http://localhost:${env.HTTP_PORT}`)
+  })
   .catch(err => { console.log(err) })
