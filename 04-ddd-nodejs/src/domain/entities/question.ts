9ff26c53520a565ccdbe157852e3e23d930739c7
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-11-05 08:53:22 -0300

    Mapeando propriedades

--- 04-ddd-nodejs/src/domain/entities/question.ts
+++ 04-ddd-nodejs/src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { randomUUID } from "node:crypto"
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId: UniqueEntityId
   title: string
   content: string
-  authorId: string
   slug: Slug
+  createdAt: Date
+  updatedAt?:Date
 }
 export class Question extends Entity<QuestionProps> { }
