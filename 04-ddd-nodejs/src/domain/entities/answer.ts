fd5d266321a030285dd24eca6366eb6dddef2ffb
alisson-moura <alisson.mo.moura@outlook.com.br>
2023-11-05 08:43:08 -0300

    Classe base de entidades

--- 04-ddd-nodejs/src/domain/entities/answer.ts
+++ 04-ddd-nodejs/src/domain/entities/answer.ts
@@ -1,20 +1,12 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 
-interface AnswerConstructor {
+interface AnswerProps{
   content: string
   authorId: string
   questionId: string
 }
-export class Answer {
-  public id: string
-  public content: string
-  public authorId: string
-  public questionId: string
-
-  constructor(props: AnswerConstructor, id?: string) {
-    this.content = props.content
-    this.authorId = props.authorId
-    this.questionId = props.questionId
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<AnswerProps> {
+  get content():string {
+    return this.props.content
   }
 }
